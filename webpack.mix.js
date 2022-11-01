302008c7746778e060a3fc795273067a9af6c209
Dougal Crowder <hi@dougal.co>
2022-11-01 10:59:09 +0000

    wishlist

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ mix.js('src/js/scripts.js', 'assets')
     .sass('src/sass/styles-page.scss', 'assets')
     .sass('src/sass/styles-product.scss', 'assets')
     .sass('src/sass/styles-swiffy.scss', 'assets')
+    .sass('src/sass/styles-searchpage.scss', 'assets')
     .setPublicPath('assets')
     .browserSync({
       proxy: 'http://127.0.0.1:9292',
